5373f1e4fbda85049fa5e555e731f3d17a41d7b2
Shrysxs <shreyashborade2@gmail.com>
2026-01-30 17:59:29 +0530

    initial test commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const app = express();
+
+let requestCount = 0;
+
+app.get("/sum", function(req, res) {
+    requestCount = requestCount + 1;
+    console.log("Total no of requests: ", +requestCount);
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
